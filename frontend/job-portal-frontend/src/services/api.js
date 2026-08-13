c6230241a77943705140ab87768bcc05eb2ef8f0
Arjun Waman <arjunwaman90@gmail.com>
2026-08-13 15:28:58 +0530

    Update frontend API URL

--- frontend/job-portal-frontend/src/services/api.js
+++ frontend/job-portal-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://192.168.1.20:5000/api",
+  baseURL: "https://online-job-portal-ai.onrender.com/api",
   withCredentials: true,
 });
 
