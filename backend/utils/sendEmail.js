9041f057cc567d202e681f95a6878a1eb42a0233
Arjun Waman <arjunwaman90@gmail.com>
2026-08-13 14:19:47 +0530

    Update Gmail transporter

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,14 +1,15 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  family: 4,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 transporter.verify((error) => {
